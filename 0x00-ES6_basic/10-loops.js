455d41c525bee90b6281b27fdf22159aa576fabe
Ayobami <oludemiayobami@gmail.com>
2024-04-09 07:44:43 +0100

    fix(lint): fix lint errors in 2, 10, 6

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = Array.from(array);
-  for (const [idx, value] of array.entries()) {
-    newArray[idx] = appendString + value;
+  const newArray = [];
+  for (const value of array.entries()) {
+    newArray.push(appendString + value);
   }
   return newArray;
 }
