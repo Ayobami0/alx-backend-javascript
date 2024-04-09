c760147652e1a8716240c89158209ea9a71d6eb5
Ayobami <oludemiayobami@gmail.com>
2024-04-09 22:28:53 +0100

    fix(checker): fix for 2 and 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,9 +3,13 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   createUser()
     .then((user) => {
-      uploadPhoto().then((photoID) => {
-        console.log(photoID.body, `${user.firstName} ${user.lastName}`);
-      });
+      uploadPhoto()
+        .then((photoID) => {
+          console.log(photoID.body, `${user.firstName} ${user.lastName}`);
+        })
+        .catch(() => {
+          console.log('Signup system offline');
+        });
     })
     .catch(() => {
       console.log('Signup system offline');
