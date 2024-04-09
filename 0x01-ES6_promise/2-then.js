6816d3bc765bd92eb8d02d8e657892f7132bd3a8
Ayobami <oludemiayobami@gmail.com>
2024-04-09 22:59:13 +0100

    fix(checker): tired

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(
-      () => ({
-        status: 200,
-        body: 'success',
-      }),
-      () => new Error(),
-    )
-    .then(() => {
-      console.log('Got a response from the API');
-    });
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .then((val) => val);
 }
