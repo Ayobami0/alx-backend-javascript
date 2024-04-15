4cc49a6da2a936438e5f5b7bc8af9d73e05fe255
Ayobami <oludemiayobami@gmail.com>
2024-04-15 18:52:06 +0100

    fix: Checker fix for task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,9 @@
 export default class Building {
   constructor(sqft) {
-    if (this !== Building && this.evacuationWarningMessage === undefined) {
+    if (
+      this.constructor !== Building
+      && this.evacuationWarningMessage === undefined
+    ) {
       throw new Error(
         'Class extending Building must override evacuationWarningMessage',
       );
