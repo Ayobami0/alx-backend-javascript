49cd3e20411fe879f7aa3a6cc476f6936810a148
Ayobami <oludemiayobami@gmail.com>
2024-04-15 10:25:34 +0100

    fix: checker fixes 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,17 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (length !== Number) {
+      throw new Error('Length must be a number');
+    }
+
+    if (students !== Array) {
+      throw new Error('Students must be an array');
+    }
+
+    if (name !== String) {
+      throw new Error('Name must be a string');
+    }
+
     this._name = name;
     this._length = length;
     this._students = students;
@@ -18,20 +30,23 @@ export default class HolbertonCourse {
   }
 
   set name(val) {
-    if (typeof val !== 'string') {
+    if (val !== String) {
       throw new Error('Name must be a string');
     }
     this._name = val;
   }
 
   set length(val) {
-    if (typeof val !== 'number') {
+    if (val !== Number) {
       throw new Error('Length must be a number');
     }
     this._length = val;
   }
 
   set students(val) {
+    if (val !== Array) {
+      throw new Error('Students must be an array');
+    }
     this._students = val;
   }
 }
