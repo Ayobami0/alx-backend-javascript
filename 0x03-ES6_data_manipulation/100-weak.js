d3f71acfa58e7ae2120cdda23e74a72189ccaaa8
Ayobami <oludemiayobami@gmail.com>
2024-04-17 10:56:56 +0100

    fix: Fix for 100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   const called = weakMap.get(endpoint);
-  if (called >= 5) {
+  if (called + 1 >= 5) {
     throw new Error('Endpoint load is high');
   }
   weakMap.set(endpoint, called === undefined ? 1 : called + 1);
