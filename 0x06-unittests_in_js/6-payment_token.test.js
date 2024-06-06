a7a1cfeeb2ae135b2cd156db4c4642e6b94989eb
Ayobami <oludemiayobami@gmail.com>
2024-06-06 17:03:46 +0100

    chore: add tasks 6

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,9 @@
+const { expect } = require("chai");
+const getPaymentTokenFromAPI = require("./6-payment_token");
+
+
+describe('getPaymentTokenFromAPI', () => {
+  it('return correct data if true', (done) => {
+    getPaymentTokenFromAPI(done(false)).then((v) => expect(v).to.include({data: 'Successful response from the API'}));
+  });
+});
